57dd33ab3647607f18b828ace32bea81833b8a96
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-05-31 22:15:36 -0400

    Update config.mjs

--- assets/js/config.mjs
+++ assets/js/config.mjs
@@ -14,10 +14,10 @@ export const config_datas = {
         youtube: "YOUTUBE",
         instagram: "INSTAGRAM",
         QA_BOX_HEADER: "WHY PEOPLE CHOOSE US?",
-        QA_BOX_ANSWER: "WERE A DAILY RISING FIVEM COMMUNITY! JOIN BSC NOW!", 
+        QA_BOX_ANSWER: "WE'RE A DAILY RISING FIVEM COMMUNITY! JOIN MRP NOW!", 
         authors: {
-            name1: "BeansFL",
-            name2: "Paul",
+            name1: "joski98",
+            name2: "zakmf",
             project1: "Owner",
             project2: "Owner",
         },
